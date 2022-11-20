144190df7d742d325086a4df4585e2b13fddb87a
DouAnd-Cutz <110060761+DouAnd-Cutz@users.noreply.github.com>
2022-11-20 01:39:44 -0600

    prueba de vista top 100 general

---
+++ resources/views/reportes/top100General.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Css/bootstrap.css">
+    <link rel="stylesheet" href="/Css/style.css">
+    <link rel="stylesheet" href="{{asset('css/>app.css')}}">
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+    <title>Store Online S.A</title>
+</head>
+<body style="background-image: url('/Assets/Image/fondo.jpg');background-attachment: fixed;">
+    <header >
+    @include('partials.navbar_admin')
+    <br>
+    <br>
+    <br>
+        
+    </header>    
+
+    <main>
+        <div class="container">
+        <a class="btn btn-primary" href="{{route('producto.create')}}">Crear</a>
+        <br>
+        <br>
+            <table class="table table-striped" style="background-color: white;">
+                <thead>
+                <tbody>
+                        <tr>
+                            
+                            <td>
+                                Nombre
+                            </td>
+                            <td>
+                                Cantidad
+                            </td>
+                        </tr>
+                    </tbody>
+
+                    @foreach($products as $producto)
+                        <tr>
+                           
+                            <td>
+                                {{$producto->name}}
+                            </td>
+                            <td>
+                                {{$producto->cantidad}}
+                            </td>
+                            
+
+                        </tr>
+                    @endforeach
+                </thead>                
+            </table>       
+        </div>
+    </main>
+</body>
+</html>
