ef5d875e8995088f1d6a53dbb0aed88a49ce09ca
Santos2102 <mikesan2102@umes.edu.gt>
2022-11-20 22:05:47 -0600

    Modificando navbar gerente

--- resources/views/partials/navbar_gerente.blade.php
+++ resources/views/partials/navbar_gerente.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary shadow-sm">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/gerente') }}">
             Store Online S.A.
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -12,60 +12,60 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/">Reportes</a>
                 </li>
+                <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false" v-pre>
+                    Top 100 Productos más vendidos
+                </a>
 
-                <div class="btn-group">
-                    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"    aria-expanded="false">
-                        Top 100 de productos más vendidos
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="/top100">General</a>
-                        <a class="dropdown-item" href="/top100PraderaChimaltenango">Pradera Chimaltenango</a>
-                        <a class="dropdown-item" href="/top100PraderaEscuintla">Pradera Escuintla</a>
-                        <a class="dropdown-item" href="/top100Mazatenango">Las Américas Mazatenango</a>
-                        <a class="dropdown-item" href="/top100LaTrinidad">La Trinidad Coatepeque</a>
-                        <a class="dropdown-item" href="/top100PraderaXela">Pradera Xela Quetzaltenango</a>
-                        <a class="dropdown-item" href="/top100Miraflores">Centro Comercial Miraflores CC</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Separated link</a>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/top100">General</a>
+                            <a class="dropdown-item" href="/top100PraderaChimaltenango">Pradera Chimaltenango</a>
+                            <a class="dropdown-item" href="/top100PraderaEscuintla">Pradera Escuintla</a>
+                            <a class="dropdown-item" href="/top100Mazatenango">Las Américas Mazatenango</a>
+                            <a class="dropdown-item" href="/top100LaTrinidad">La Trinidad Coatepeque</a>
+                            <a class="dropdown-item" href="/top100PraderaXela">Pradera Xela Quetzaltenango</a>
+                            <a class="dropdown-item" href="/top100Miraflores">Centro Comercial Miraflores CC</a>
                 </div>
+            </li>
         </div>
 
 
-            </ul>
-            <ul class="navbar-nav ms-auto">
-                <!-- Authentication Links -->
-                @guest
-                @if (Route::has('login'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @endif
+        </ul>
+        <ul class="navbar-nav ms-auto">
+            <!-- Authentication Links -->
+            @guest
+            @if (Route::has('login'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @endif
 
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-                @endif
-                @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
+            @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            @endif
+            @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
                     </a>
 
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-                @endguest
-            </ul>
-        </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+            @endguest
+        </ul>
+    </div>
     </div>
 </nav>
